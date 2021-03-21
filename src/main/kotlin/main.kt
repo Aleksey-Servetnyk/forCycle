1dcb1030abc224be52f99a21c949b06938d19091
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2021-03-21 23:13:41 +0200

    chapter 2.5.1..2.5.2 --try, catch, finally--

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,34 @@
-import java.lang.IllegalArgumentException
+import java.io.BufferedReader
+import java.io.StringReader
+import java.lang.NumberFormatException
 
 fun main(args: Array<String>) {
-    percentValidator(50)
-    percentValidator(150)
-    println(percentValidatorInExpr(50))
-    println(percentValidatorInExpr(150))
+    val reader = BufferedReader(StringReader("239"))
+    println(readNumber(reader))
+
+    val readerExprDigit = BufferedReader(StringReader("456"))
+    readNumberExpr(readerExprDigit)
+    val readerExpr = BufferedReader(StringReader("Not a number"))
+    readNumberExpr(readerExpr)
 }
 
-fun percentValidator(num: Int) {
-    if (num !in 0..100) throw IllegalArgumentException("A percentage value mast be between 0 and 100: $num")
+fun readNumber(reader: BufferedReader): Int? {
+    return try {
+        val line = reader.readLine()
+        Integer.parseInt(line)
+    } catch (e: NumberFormatException) {
+        null
+    } finally {
+        reader.close()
+    }
 }
 
-fun percentValidatorInExpr(num: Int): Boolean {
-    return if (num in 0..100) true
-    else throw IllegalArgumentException("A percentage value mast be between 0 and 100: $num")
+fun readNumberExpr(reader: BufferedReader) {
+    val number = try {
+        Integer.parseInt(reader.readLine())
+    } catch (e: NumberFormatException) {
+        //return
+        null
+    }
+    println(number)
 }
