7b27397bf0acaf49ad34aec401b91ee68c81f864
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2021-03-21 22:10:01 +0200

    using "in" to test for entry into a range or collection - 2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,17 @@
 fun main(args: Array<String>) {
     println(isLetter('q'))
     println(isNotDigit('x'))
+    println(recognize('8'))
+    println("Kotlin" in "Java".."Scala")
+    println("Kotlin" in setOf("Java", "Scala"))
 }
 
 fun isLetter(letter: Char) = letter in 'a'..'z' || letter in 'A'..'Z'
 fun isNotDigit(letter: Char) = letter !in '0'..'9'
+
+fun recognize(letter: Char) =
+    when (letter) {
+        in '0'..'9' ->                  "It`s a digit."
+        in 'a'..'z', in 'A'..'Z' ->     "It`s a letter."
+        else ->                         "I don`t know..."
+    }
