6fbbcf1be756f3b185c40a564cd066062e701ce2
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2021-03-22 08:40:00 +0200

    creating collections and calling functions

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,32 +3,52 @@ import java.io.StringReader
 import java.lang.NumberFormatException
 
 fun main(args: Array<String>) {
-    val reader = BufferedReader(StringReader("239"))
-    println(readNumber(reader))
+    val set = hashSetOf(1, 7, 53)
+    val list = arrayListOf(1, 7, 53)
+    val map = hashMapOf(1 to "one", 7 to "seven", 53 to "fifty-three")
 
-    val readerExprDigit = BufferedReader(StringReader("456"))
-    readNumberExpr(readerExprDigit)
-    val readerExpr = BufferedReader(StringReader("Not a number"))
-    readNumberExpr(readerExpr)
+    println(set.javaClass)
+    println(list.javaClass)
+    println(map.javaClass)
+
+    println(list)
+    println(joinToString(list,"; ", "(", ")"))
+    println(joinToString(list,separator = "; ", prefix = "(", postfix = ")"))
+    println(joinToStringWithDefaultParameters(list))
+    println(joinToStringWithDefaultParameters(list, "| "))
+    println(joinToStringWithDefaultParameters(list, postfix = ";", prefix = "# "))
+
 }
 
-fun readNumber(reader: BufferedReader): Int? {
-    return try {
-        val line = reader.readLine()
-        Integer.parseInt(line)
-    } catch (e: NumberFormatException) {
-        null
-    } finally {
-        reader.close()
+@JvmOverloads
+fun <T> joinToString(
+    collection: Collection<T>,
+    separator: String,
+    prefix: String,
+    postfix: String
+): String {
+    val result = StringBuilder(prefix)
+
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
     }
+    result.append(postfix)
+    return result.toString()
 }
 
-fun readNumberExpr(reader: BufferedReader) {
-    val number = try {
-        Integer.parseInt(reader.readLine())
-    } catch (e: NumberFormatException) {
-        //return
-        null
+fun <T> joinToStringWithDefaultParameters(
+    collection: Collection<T>,
+    separator: String = ", ",
+    prefix: String = "",
+    postfix: String = ""
+): String {
+    val result = StringBuilder(prefix)
+
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
     }
-    println(number)
+    result.append(postfix)
+    return result.toString()
 }
