cbbb3f9604dfedab283b8e7a2e415492a7e96bb9
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2021-04-03 04:24:10 +0300

    modifiers

---
+++ src/main/kotlin/classModificators/TalkativeButton.kt
@@ -0,0 +1,13 @@
+package classModificators
+
+import interfaces.Focusable
+
+internal open class TalkativeButton : Focusable {
+    private fun yell() = println("hey!")
+    protected fun whisper() = println("Let`s talk!")
+}
+
+fun TalkativeButton.giveSpeech() {
+    yell()
+    whisper()
+}
