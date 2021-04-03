8d5187eb6a25632b2bc88831916d031b6abfb14f
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2021-04-03 11:45:09 +0300

    universal object methods

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,14 @@
-import accessorMethods424.logMessage
-import changingTheVisibilityOfAccessors.LengthCounter
+import universalObjectMethods431.Client
 
 fun main(args: Array<String>) {
-   val lengthCounter = LengthCounter()
-    lengthCounter.addWord("Hi!")
-    println(lengthCounter.counter)
+    val client1 = Client("Alice", 342562)
+    println(client1)
+
+    val client2 = Client("Alice", 342562)
+    println(client1 == client2)
+
+    val processed = hashSetOf(Client("Alice", 342562))
+    println(processed.contains(Client("Alice", 342562)))
+
 }
 
