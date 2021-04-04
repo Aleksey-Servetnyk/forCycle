51a76ee81b213a2b2f521d2ba32224f670a8fc5a
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2021-04-04 07:10:28 +0300

    lambda : access to variables from a context

---
+++ src/main/kotlin/lambdaAccessToVariables514/Arguments.kt
@@ -0,0 +1,19 @@
+package lambdaAccessToVariables514
+
+fun printMessagesWithPrefix(messages: Collection<String>, prefix: String) {
+    messages.forEach {
+        println("$prefix $it")
+    }
+}
+fun printProblemCounts(responses: Collection<String>) {
+    var clientErrors = 0
+    var serverErrors = 0
+    responses.forEach {
+        if (it.startsWith("4")) {
+            clientErrors++
+        } else if (it.startsWith("5")) {
+            serverErrors++
+        }
+    }
+    println("$clientErrors client errors, $serverErrors server errors")
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,37 +1,12 @@
-import lambdaCollections512.Person
+import lambdaAccessToVariables514.printMessagesWithPrefix
+import lambdaAccessToVariables514.printProblemCounts
 
 fun main(args: Array<String>) {
-    val sum = { x: Int, y: Int -> x +y }
-    println(sum(1, 2));
+    val errors = listOf("403 Forbidden", "404 Not Found")
+    printMessagesWithPrefix(errors, "Error: ")
 
-    { println(42) }()
-    run { println(42) }
-
-    val people = listOf(Person("Alice", 29), Person("Bob", 31))
-    println(people.maxByOrNull { it.age })
-    println(people.maxByOrNull { p: Person -> p.age })
-
-    // lambda in the named argument
-    val names = people.joinToString(separator = " ", transform = {p: Person -> p.name})
-    println(names)
-
-    // lambda outside brackets
-    println(people.joinToString(" ") { p: Person -> p.name })
-
-    // remove the parameter type
-    println(people.joinToString(" ") { p -> p.name })
-
-    // parameter by default
-    println(people.maxByOrNull { it.age })
-
-    // multi play lambda
-    val sum1 = { x: Int, y: Int ->
-        println("Computing the sum of $x and $y")
-        x + y
-    }
-    println(sum1(1, 2));
-
-
+    val responses = listOf("200 Ok", "418 I`m a teapot", "500 Internal Server Error")
+    printProblemCounts(responses)
 }
 
 
