6955a6b64a0e47d3a526b1670e7bdad8584ac72e
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2021-04-04 09:24:35 +0300

    collections: groupBy function

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,11 @@
 import lambdaCollections512.Person
 
 fun main(args: Array<String>) {
-    val canBeInClub = {p: Person -> p.age <= 27}
-    val people = listOf(Person("Alice", 27), Person("Bob", 31))
-    println(people.all(canBeInClub))
-    println(people.any(canBeInClub))
-    println(people.count(canBeInClub))
-    println(people.find(canBeInClub))
+    val people = listOf(Person("Alice", 31), Person("Bob", 29), Person("Carol", 31))
+    println(people.groupBy { it.age })
 
-    val list = listOf(1, 2, 3)
-    println(!list.all { it == 3 })
-    println(list.any { it != 3 })
+    val list = listOf("a", "ab", "b")
+    println(list.groupBy(String::first))
 
 }
 
