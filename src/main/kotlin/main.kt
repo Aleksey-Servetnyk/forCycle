2bc7e122ec9d53aa7dca6bbf5291aa40730c409d
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2021-04-04 09:18:41 +0300

    applying ALL, ANY, COUNT, FIND predicates to collections

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,23 +1,16 @@
 import lambdaCollections512.Person
 
 fun main(args: Array<String>) {
-    val list = listOf(1, 2, 3, 4)
-    println(list.filter { it %2 == 0 })
-    println(list.map { it * it })
+    val canBeInClub = {p: Person -> p.age <= 27}
+    val people = listOf(Person("Alice", 27), Person("Bob", 31))
+    println(people.all(canBeInClub))
+    println(people.any(canBeInClub))
+    println(people.count(canBeInClub))
+    println(people.find(canBeInClub))
 
-    val people = listOf(Person("Alice", 29), Person("Bob", 31))
-    println(people.filter { it.age > 30 })
-    println(people.map { it.name })
-    println(people.map(Person::age))
-    println(people.filter { it.age > 30 }.map(Person::name))
-
-    println(people.filter { it.age == people.maxByOrNull(Person::age)?.age})
-    val maxAge = people.maxByOrNull(Person::age)?.age
-    println(people.filter { it.age == maxAge })
-
-    val numbers = mapOf(0 to "zero", 1 to "one")
-    println(numbers.mapValues { it.value.toUpperCase() })
-    println(numbers.mapKeys { it.key })
+    val list = listOf(1, 2, 3)
+    println(!list.all { it == 3 })
+    println(list.any { it != 3 })
 
 }
 
