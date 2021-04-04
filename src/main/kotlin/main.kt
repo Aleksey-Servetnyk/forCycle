a74384d49344f60df8a98ddfa053a4b8463b5cc6
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2021-04-04 07:47:57 +0300

    lambda : references to class members

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,25 @@
-import lambdaAccessToVariables514.printMessagesWithPrefix
-import lambdaAccessToVariables514.printProblemCounts
+import lambdaCollections512.Person
 
 fun main(args: Array<String>) {
-    val errors = listOf("403 Forbidden", "404 Not Found")
-    printMessagesWithPrefix(errors, "Error: ")
+    val person = Person("Bob", 39)
+    val getAge = person::age
+    println(getAge)
 
-    val responses = listOf("200 Ok", "418 I`m a teapot", "500 Internal Server Error")
-    printProblemCounts(responses)
+    run (::salute)
+
+    val createPerson = ::Person
+    val p = createPerson("Alice", 29)
+    println(p)
+
+    val predicate = p::isAdult
+    println(predicate)
+
+    val ageFun = p::age
+    println(ageFun())
 }
+
+fun salute() = println("Salute!")
+fun Person.isAdult() = age >= 21
 
 
 
